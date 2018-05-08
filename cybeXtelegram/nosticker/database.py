534f1feb428eb1ad7123a5c2f3d1f6aaa1375a88
staniya <staniya@thacher.org>
2018-05-08 16:32:58 +0800

    fixing requirements.txt

--- cybeXtelegram/nosticker/database.py
+++ cybeXtelegram/nosticker/database.py
@@ -1,7 +1,23 @@
 from pymongo import MongoClient
+from sshtunnel import SSHTunnelForwarder
+
+MONGO_HOST = "47.52.20.196"
+MONGO_DB = "cybex_user_management"
+MONGO_USER = "staniya"
+MONGO_PASS = "password"
+
+
+# server = SSHTunnelForwarder(
+#     MONGO_HOST,
+#     ssh_username=MONGO_USER,
+#     ssh_passsword=MONGO_PASS,
+#     remote_bind_address=('127.0.0.1', 27017)
+# )
 
 
 def connect_db():
+    # server.start()
+    # client = MongoClient('127.0.0.1', server.local_bind_port)
     db = MongoClient()['cybex_user_management']
     db.user.create_index('username', unique=True)
     db.joined_user.create_index([('chat_id', 1), ('user_id', 1)])
